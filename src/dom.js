2a71ca7043bc9171f2a766e7da9080495d798bed
William <william.jiang1101@gmail.com>
2026-02-18 14:20:12 -0500

    Completed top media handlers

--- src/dom.js
+++ src/dom.js
@@ -1,9 +1,10 @@
-export const renderTopMedias = (mediaType, data) => {
-  const ul = document.querySelector(`#${mediaType.toLowerCase()}-list`);
+export const renderTopMedias = (data) => {
+  const ul = document.querySelector(`#media-list`);
+  ul.innerHTML = '';
   const topList = data.slice(0, 10);
   topList.forEach((media) => {
     const li = document.createElement('li');
-    li.dataset.malId = data.mal_id;
+    li.dataset.malId = media.mal_id;
     const img = document.createElement('img');
     img.src = media.images.webp.image_url;
     img.alt = `${media.title} image`;
