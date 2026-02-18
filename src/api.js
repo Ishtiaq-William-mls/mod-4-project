deb651e130460efebcdc9bf491c91fbea8215329
ishtiaqa1 <ishtiaqa2003@gmail.com>
2026-02-18 12:18:49 -0500

    Added fetch helpers for manga and anime

--- src/api.js
+++ src/api.js
@@ -1,6 +1,26 @@
-const getSearch = async (query) => {
+const getMangaSearch = async (query) => {
     try {
         const data = await fetch(`https://api.jikan.moe/v4/manga?q=${query}`);
+        if (!data.ok) {
+            throw Error(`Fetch failed: ${response.status} ${response.statusText}`);
+        }
+        const response = await data.json()
+        return {
+            data: response.data,
+            error: null
+        }
+    } catch (err) {
+        console.warn(err);
+        return {
+            data: null,
+            error: err
+        }
+    }
+}
+
+const getAnimeSearch = async (query) => {
+    try {
+        const data = await fetch(`https://api.jikan.moe/v4/anime?q=${query}`);
         if (!data.ok) {
             throw Error(`Fetch failed: ${response.status} ${response.statusText}`);
         }
