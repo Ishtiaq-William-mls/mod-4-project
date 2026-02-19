db59964909ade66d809d1696ced7db166098d458
William <william.jiang1101@gmail.com>
2026-02-19 10:40:15 -0500

    Made changes to search and added filtering

--- src/app.js
+++ src/app.js
@@ -1,25 +1,32 @@
-import { renderSearch, renderTopMedias } from './dom.js';
+import { renderSearch, renderTopMedias, counter } from './dom.js';
 import { getSearch, getTopMedias } from './api.js';
 
 const form = document.getElementById('search-form');
+const selector = document.querySelector('#sort-media');
+const topMedia = document.querySelector('#top-media');
+let mediaType = 'anime';
+let searching;
+let query;
+let search;
 
 form.addEventListener('submit', async (e) => {
   e.preventDefault();
-  const query = document.getElementById('query').value.trim();
-  const mediaType = document.querySelector('input[name="media"]:checked').value;
+  query = document.getElementById('query').value.trim();
+  //   const mediaType = document.querySelector('input[name="media"]:checked').value;
   const search = await getSearch(`${mediaType}?q=${query}`);
   if (search.error) {
     console.warn(search.error.message);
     return;
+  } else if (search.data.length - counter === 0) {
+    topMedia.textContent = `No Search Results (${search.data.length}) for ${query}`;
   }
+  searching = true;
   renderSearch(search.data);
+  topMedia.textContent = `Top (${mediaType}) Search Results (${search.data.length - counter}) for ${query}`;
 });
 
-const selector = document.querySelector('#sort-media');
-const topMedia = document.querySelector('#top-media');
-let mediaType;
-
-getTopMedias('anime').then((response) => {
+getTopMedias(mediaType).then((response) => {
+  searching = false;
   if (response.error) {
     console.warn(response.error.message);
     return;
@@ -27,18 +34,38 @@ getTopMedias('anime').then((response) => {
   renderTopMedias(response.data);
 });
 
-selector.addEventListener('change', (event) => {
+selector.addEventListener('change', async (event) => {
   mediaType = event.target.value;
 
   console.log(mediaType);
-  getTopMedias(mediaType).then((response) => {
-    if (response.error) {
-      console.warn(response.error.message);
-      return;
-    }
-    mediaType =
-      mediaType[0].toUpperCase() + mediaType.slice(1, mediaType.length);
-    renderTopMedias(response.data);
-    topMedia.textContent = `Top ${mediaType}`;
-  });
+  //   getTopMedias(mediaType).then((response) => {
+  //     if (response.error) {
+  //       console.warn(response.error.message);
+  //       return;
+  //     }
+  //     mediaType =
+  //       mediaType[0].toUpperCase() + mediaType.slice(1, mediaType.length);
+  //     if (searching) {
+  //       search = await getSearch(`${mediaType}?q=${query}`)
+  //       renderSearch(search.data);
+  //       return;
+  //     }
+  //     renderTopMedias(response.data);
+  //     topMedia.textContent = `Top ${mediaType}`;
+  //   });
+  const response = await getTopMedias(mediaType);
+  if (response.error) {
+    console.warn(response.error.message);
+    return;
+  }
+  if (searching) {
+    search = await getSearch(`${mediaType}?q=${query}`);
+    renderSearch(search.data);
+    topMedia.textContent = `Top (${mediaType}) Search Results (${search.data.length - counter}) for ${query}`;
+    return;
+  }
+  let displayName =
+    mediaType[0].toUpperCase() + mediaType.slice(1, mediaType.length);
+  renderTopMedias(response.data);
+  topMedia.textContent = `Top ${displayName}`;
 });
--- src/dom.js
+++ src/dom.js
@@ -1,36 +1,46 @@
 const searchList = document.querySelector('#search-list');
+const ul = document.querySelector(`#media-list`);
+export let counter;
 
 export const renderSearch = (results) => {
-  searchList.innerHTML = '';
+  //   searchList.innerHTML = '';
+  ul.innerHTML = '';
+  counter = 0;
   results.forEach((r) => {
+    const genres = new Set(r.genres.map((genre) => genre.name));
+    if (genres.has('Hentai') || genres.has('Erotica') || genres.has('Ecchi')) {
+      counter++;
+      return;
+    }
     const li = document.createElement('li');
-
+    li.dataset.malId = r.mal_id;
+    li.classList.add('anime-card');
     const img = document.createElement('img');
-    img.src = r.images.webp.image_url;
+    const title = document.createElement('h3');
+    title.textContent = r.title_english ? r.title_english : r.title;
+    img.src = r.images.webp.large_image_url;
     img.alt = r.title;
 
-    const h3 = document.createElement('h3');
-    h3.textContent = r.title;
-
-    const info = document.createElement('p');
-    info.textContent = `${r.synopsis}`;
-
-    li.append(img, h3, info);
-    searchList.append(li);
+    li.append(img, title);
+    // searchList.append(li);
+    ul.append(li);
   });
 };
 export const renderTopMedias = (data) => {
-  const ul = document.querySelector(`#media-list`);
   ul.innerHTML = '';
   const topList = data.slice(0, 10);
   topList.forEach((media) => {
     const li = document.createElement('li');
     li.dataset.malId = media.mal_id;
+    li.classList.add('anime-card');
     const img = document.createElement('img');
+    const title = document.createElement('h3');
+    title.textContent = media.title_english ? media.title_english : media.title;
     img.classList.add('anime-images');
     img.src = media.images.webp.large_image_url;
     img.alt = `${media.title} image`;
-    li.append(img);
+
+    li.append(img, title);
     ul.append(li);
   });
 };
