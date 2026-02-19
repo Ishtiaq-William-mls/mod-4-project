08fae63c997bdf5973eacf0ca2fb8bb85f4fc823
William <william.jiang1101@gmail.com>
2026-02-19 08:53:41 -0500

    added basic styling

--- src/app.js
+++ src/app.js
@@ -1,18 +1,18 @@
-import { renderSearch, renderTopMedias } from "./dom.js";
-import { getSearch, getTopMedias } from "./api.js";
+import { renderSearch, renderTopMedias } from './dom.js';
+import { getSearch, getTopMedias } from './api.js';
 
-const form = document.getElementById("search-form");
+const form = document.getElementById('search-form');
 
-form.addEventListener("submit", async (e) => {
-    e.preventDefault();
-    const query = document.getElementById('query').value.trim();
-    const mediaType = document.querySelector('input[name="media"]:checked').value;
-    const search = await getSearch(`${mediaType}?q=${query}`)
-    if(search.error){
-        console.warn(search.error.message);
-        return;
-    }
-    renderSearch(search.data);
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  const query = document.getElementById('query').value.trim();
+  const mediaType = document.querySelector('input[name="media"]:checked').value;
+  const search = await getSearch(`${mediaType}?q=${query}`);
+  if (search.error) {
+    console.warn(search.error.message);
+    return;
+  }
+  renderSearch(search.data);
 });
 
 const selector = document.querySelector('#sort-media');
@@ -29,11 +29,15 @@ getTopMedias('anime').then((response) => {
 
 selector.addEventListener('change', (event) => {
   mediaType = event.target.value;
+
+  console.log(mediaType);
   getTopMedias(mediaType).then((response) => {
     if (response.error) {
       console.warn(response.error.message);
       return;
     }
+    mediaType =
+      mediaType[0].toUpperCase() + mediaType.slice(1, mediaType.length);
     renderTopMedias(response.data);
     topMedia.textContent = `Top ${mediaType}`;
   });
--- src/dom.js
+++ src/dom.js
@@ -1,25 +1,24 @@
-const searchList = document.querySelector('#search-list')
-
+const searchList = document.querySelector('#search-list');
 
 export const renderSearch = (results) => {
-    searchList.innerHTML = '';
-    results.forEach((r) => {
-        const li = document.createElement('li');
+  searchList.innerHTML = '';
+  results.forEach((r) => {
+    const li = document.createElement('li');
 
-        const img = document.createElement('img');
-        img.src = r.images.webp.image_url;
-        img.alt = r.title;
+    const img = document.createElement('img');
+    img.src = r.images.webp.image_url;
+    img.alt = r.title;
 
-        const h3 = document.createElement('h3');
-        h3.textContent = r.title;
+    const h3 = document.createElement('h3');
+    h3.textContent = r.title;
 
-        const info = document.createElement('p');
-        info.textContent = `${r.synopsis}`;
+    const info = document.createElement('p');
+    info.textContent = `${r.synopsis}`;
 
-        li.append(img, h3, info);
-        searchList.append(li);
-    })
-}
+    li.append(img, h3, info);
+    searchList.append(li);
+  });
+};
 export const renderTopMedias = (data) => {
   const ul = document.querySelector(`#media-list`);
   ul.innerHTML = '';
@@ -28,7 +27,8 @@ export const renderTopMedias = (data) => {
     const li = document.createElement('li');
     li.dataset.malId = media.mal_id;
     const img = document.createElement('img');
-    img.src = media.images.webp.image_url;
+    img.classList.add('anime-images');
+    img.src = media.images.webp.large_image_url;
     img.alt = `${media.title} image`;
     li.append(img);
     ul.append(li);
