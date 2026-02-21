9e1c665eb8c2cf05a0a6f3dcebf21b567a2b5b32
William <william.jiang1101@gmail.com>
2026-02-20 22:32:22 -0500

    added local storage for favoriting and keeping media selection, also made some extra fixes to some functions

--- src/api.js
+++ src/api.js
@@ -1,6 +1,10 @@
+// import { mediaType } from './app';
+
 export const getSearch = async (query) => {
   try {
-    const data = await fetch(`https://api.jikan.moe/v4/${query}`);
+    const data = await fetch(
+      `https://api.jikan.moe/v4/${query}&genres_exclude=12,49,9`,
+    );
     if (!data.ok) {
       throw Error(`Fetch failed: ${data.status} ${data.statusText}`);
     }
@@ -56,26 +60,38 @@ export const getById = async (mediaId) => {
 
 export const getRandom = async (mediaType) => {
   try {
+    const randomPage = Math.floor(Math.random() * 50) + 1;
     const response = await fetch(
-      `https://api.jikan.moe/v4/random/${mediaType}`,
+      `https://api.jikan.moe/v4/${mediaType}?page=${randomPage}&order_by=popularity&sort=asc&genres_exclude=12,49`,
     );
     if (!response.ok) {
       throw new Error(
         `Fetch failed: ${response.status} ${response.statusText}`,
       );
     }
-    const animeData = await response.json();
-    return { data: animeData.data, error: null };
+    const data = await response.json();
+    const animeData = data.data[Math.floor(Math.random() * data.data.length)];
+    console.log(data);
+    console.log(animeData);
+    return { data: animeData, error: null };
   } catch (error) {
     return { data: null, error: error };
   }
 };
 
-export const getOngoing = async () => {
+export const getOngoing = async (mediaType) => {
   try {
-    const response = await fetch(
-      `https://api.jikan.moe/v4/seasons/now`,
-    );
+    let response;
+    const randomPage = Math.floor(Math.random() * 6) + 1;
+    if (mediaType === 'manga') {
+      response = await fetch(
+        `https://api.jikan.moe/v4/manga?status=publishing&order_by=popularity&sort=asc&page=${randomPage}&genres_exclude=12,49,9`,
+      );
+    } else {
+      response = await fetch(
+        `https://api.jikan.moe/v4/anime?status=airing&order_by=popularity&sort=asc&page=${randomPage}&genres_exclude=12,49,9`,
+      );
+    }
     if (!response.ok) {
       throw new Error(
         `Fetch failed: ${response.status} ${response.statusText}`,
--- src/app.js
+++ src/app.js
@@ -14,33 +14,50 @@ import {
   getOngoing,
 } from './api.js';
 
+import {
+  getFavorites,
+  saveFavorites,
+  getMediaType,
+  saveMediaType,
+} from './storage.js';
+
 const form = document.getElementById('search-form');
 const selector = document.querySelector('#sort-media');
 const topMedia = document.querySelector('#top-media');
 // const mediaList = document.querySelector('.lists');
 const bottomSection = document.querySelector('#bottom-section');
+const bottomMedia = document.querySelector('#bottom-media');
 const closeBtn = document.querySelector('#close-btn');
 const modal = document.querySelector('#modal');
-export let mediaType = 'anime';
+let mediaType = getMediaType();
+let displayName =
+  mediaType[0].toUpperCase() + mediaType.slice(1, mediaType.length) + 's';
+topMedia.textContent = `Top ${displayName}`;
+bottomMedia.textContent = `Ongoing ${displayName}`;
+selector.value = mediaType;
 let searching;
 let query;
 
-form.addEventListener('submit', async (e) => {
-  e.preventDefault();
-  query = form.elements.query.value.trim();
+const search = async () => {
   //   const mediaType = document.querySelector('input[name="media"]:checked').value;
   const search = await getSearch(`${mediaType}?q=${query}`);
   if (search.error) {
     console.warn(search.error.message);
     return;
   }
-  searching = true;
   renderSearch(search.data);
   if (search.data.length - counter === 0) {
     topMedia.textContent = `No Search Results (${search.data.length - counter}) for ${query}`;
   } else {
     topMedia.textContent = `Top (${mediaType}) Search Results (${search.data.length - counter}) for ${query}`;
   }
+};
+
+form.addEventListener('submit', async (e) => {
+  e.preventDefault();
+  query = form.elements.query.value.trim();
+  await search();
+  searching = true;
   form.reset();
 });
 
@@ -60,16 +77,17 @@ loadTopMedias();
 
 selector.addEventListener('change', async (event) => {
   mediaType = event.target.value;
+  saveMediaType(mediaType);
   rand.innerHTML = '';
-  if (mediaType === 'anime') {
-    bottomSection
-      .querySelectorAll('.child')
-      .forEach((child) => child.classList.remove('hidden'));
-  } else if (mediaType === 'manga') {
-    bottomSection.querySelectorAll('.child').forEach((child) => {
-      child.classList.add('hidden');
-    });
-  }
+  //   if (mediaType === 'anime') {
+  //     bottomSection
+  //       .querySelectorAll('.child')
+  //       .forEach((child) => child.classList.remove('hidden'));
+  //   } else if (mediaType === 'manga') {
+  //     bottomSection.querySelectorAll('.child').forEach((child) => {
+  //       child.classList.add('hidden');
+  //     });
+  //   }
   console.log(mediaType);
   //   getTopMedias(mediaType).then((response) => {
   //     if (response.error) {
@@ -86,32 +104,27 @@ selector.addEventListener('change', async (event) => {
   //     renderTopMedias(response.data);
   //     topMedia.textContent = `Top ${mediaType}`;
   //   });
+  const response2 = await getOngoing(mediaType);
+  displayName =
+    mediaType[0].toUpperCase() + mediaType.slice(1, mediaType.length) + 's';
+
+  if (searching) {
+    await search();
+    renderOngoing(response2.data);
+    bottomMedia.textContent = `Ongoing ${displayName}`;
+    return;
+  }
   const response = await getTopMedias(mediaType);
-  if (response.error) {
-    console.warn(response.error.message);
+  if (response.error || response2.error) {
+    response.error ? console.warn(response.error.message) : 0;
+    response2.error ? console.warn(response2.error.message) : 0;
     return;
   }
 
-  if (searching) {
-    // Should turn this into a function to reduce repetition
-    let search = await getSearch(`${mediaType}?q=${query}`);
-    if (search.error) {
-      console.warn(search.error.message);
-      return;
-    }
-    renderSearch(search.data);
-    if (search.data.length - counter === 0) {
-      topMedia.textContent = `No Search Results (${search.data.length - counter}) for ${query}`;
-      return;
-    } else {
-      topMedia.textContent = `Top (${mediaType}) Search Results (${search.data.length - counter}) for ${query}`;
-      return;
-    }
-  }
-  let displayName =
-    mediaType[0].toUpperCase() + mediaType.slice(1, mediaType.length) + 's';
   renderTopMedias(response.data);
+  renderOngoing(response2.data);
   topMedia.textContent = `Top ${displayName}`;
+  bottomMedia.textContent = `Ongoing ${displayName}`;
 });
 
 // mediaList.addEventListener('click', async (event) => {
@@ -137,12 +150,17 @@ const mediaLists = document.querySelectorAll('.lists');
 
 mediaLists.forEach((list) => {
   list.addEventListener('click', async (event) => {
+    if (event.target.closest('.favorite-btn')) {
+      return;
+    }
     document
       .querySelectorAll('.anime-card')
       .forEach((c) => c.classList.remove('selected'));
 
     const clickedLi = event.target.closest('li');
-    if (!clickedLi) return;
+    if (!clickedLi) {
+      return;
+    }
 
     clickedLi.classList.add('selected');
     const id = clickedLi.dataset.malId;
@@ -199,27 +217,31 @@ randBtn.addEventListener('click', async () => {
   randBtn.textContent = 'Loading...';
   try {
     let random;
-    let genres;
-    let explicit = true;
-    while (explicit) {
-      random = await getRandom(mediaType);
-      if (random.error) {
-        console.warn(random.error.message);
-        return;
-      }
-      genres = new Set(random.data.genres.map((genre) => genre.name));
-      if (
-        !genres.has('Hentai') &&
-        !genres.has('Erotica') &&
-        random.data.type !== 'Music'
-      ) {
-        explicit = false;
-      } else {
-        console.log(genres);
-        await sleep(800 + Math.random() * 400);
-      }
+    // let genres;
+    // let explicit = true;
+    random = await getRandom(mediaType);
+    if (random.error) {
+      console.warn(random.error.message);
+      return;
     }
-    await sleep(1500);
+    // while (explicit) {
+    //   random = await getRandom();
+    //   if (random.error) {
+    //     console.warn(random.error.message);
+    //     return;
+    //   }
+    //   genres = new Set(random.data.genres.map((genre) => genre.name));
+    //   if (
+    //     random.data.type !== 'Music'
+    //   ) {
+    //     explicit = false;
+    //     await sleep(800 + Math.random() * 400);
+    //   } else {
+    //     console.log(genres);
+    //     await sleep(800 + Math.random() * 400);
+    //   }
+    // }
+    await sleep(800 + Math.random() * 400);
     renderRandom(random.data);
   } finally {
     randBtn.textContent = 'Get Random Anime/Manga';
@@ -230,7 +252,7 @@ randBtn.addEventListener('click', async () => {
 });
 
 const loadOngoing = async () => {
-  const response = await getOngoing();
+  const response = await getOngoing(mediaType);
 
   if (response.error) {
     console.warn(response.error.message);
@@ -244,7 +266,10 @@ loadOngoing();
 
 const rand = document.querySelector('#random-media');
 
-rand.addEventListener('click', async () => {
+rand.addEventListener('click', async (event) => {
+  if (event.target.closest('.favorite-btn')) {
+    return;
+  }
   const id = rand.dataset.malId;
   const input = `${mediaType}/${id}`;
   const response = await getById(input);
@@ -257,4 +282,37 @@ rand.addEventListener('click', async () => {
   renderModalContent(response.data);
   modal.classList.remove('hidden');
   document.body.classList.add('no-scroll');
+});
+
+document.addEventListener('click', (event) => {
+  if (event.target.closest('.favorite-btn')) {
+    const heart = event.target.closest('.favorite-btn');
+
+    let favorites = getFavorites();
+
+    const container = heart.closest('[data-mal-id]');
+    if (!container) return;
+
+    const id = Number(container.dataset.malId);
+
+    if (favorites.has(id)) {
+      favorites.delete(id);
+    } else {
+      favorites.add(id);
+    }
+
+    saveFavorites(favorites);
+
+    document
+      .querySelectorAll(`[data-mal-id='${id}'] .favorite-btn`)
+      .forEach((favorite) => {
+        if (favorites.has(id)) {
+          favorite.classList.remove('fa-regular');
+          favorite.classList.add('fa-solid');
+        } else {
+          favorite.classList.remove('fa-solid');
+          favorite.classList.add('fa-regular');
+        }
+      });
+  }
 });
